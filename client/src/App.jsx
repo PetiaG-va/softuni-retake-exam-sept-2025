febcf73a77008a82143cc996d906216598dcb4c0
Petia Georgieva <petiaG-va@outlook.com>
2025-12-17 20:01:38 +0200

    create home component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,22 @@
+import { Route, Routes } from "react-router"
 import Header from "./components/header/Header.jsx"
 
 function App() {
 	return (
 		<>
+		<div className="app-wrapper">
 			<Header />
+			<main>
+				<div className="container">
+			<Routes>
+				
+			</Routes>
+
+				</div>
+			</main>
+
+		</div>
+
 			
 		</>
 	)
---
+++ client/src/components/home/Home.jsx
@@ -0,0 +1,13 @@
+export default function Home() {
+    return (
+        <div id="home-page">
+            <div className="hero">
+                <h1 id="home-title">Plant Care</h1>
+                <p id="home-subtitle">Take care of your green friends</p>
+            </div>
+            <div className="cards-grid" id="home-plants">
+                
+            </div>
+        </div>
+    )
+}

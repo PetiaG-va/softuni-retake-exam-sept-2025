1c18c64ee09b531d581df5972110b35079ad9458
Petia Georgieva <petiaG-va@outlook.com>
2025-12-17 19:37:13 +0200

    create header component

---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,20 @@
+
+
+export default function Header() {
+    return (
+        <nav>
+            <div className="nav-container">
+                <Link to="/" className="logo" data-nav="home" id="nav-logo">🌱 Plant Care</Link>
+                <div className="nav-links">
+                    <Link to="/" data-nav="home" className="active">Home</Link>
+                    <Link to="/plants" data-nav="catalog">Plants</Link>
+
+                    <div className="user-section" id="auth-section">
+                        <Link to="/login" data-nav="login">Login</Link>
+                        <Link to="/register" data-nav="register">Register</Link>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    )
+}

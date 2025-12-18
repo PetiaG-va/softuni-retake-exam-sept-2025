8054dee19d7f836e35226e407287565f58bd99f7
Petia Georgieva <petiaG-va@outlook.com>
2025-12-18 14:17:20 +0200

    fix home css

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,13 +1,41 @@
+import { Link } from "react-router";
+
 export default function Home() {
     return (
-        <div id="home-page">
-            <div className="hero">
-                <h1 id="home-title">Plant Care</h1>
-                <p id="home-subtitle">Take care of your green friends</p>
+        // 
+        <section className="home-page">
+            <div className="container">
+                <div className="hero">
+                    <h1 id="home-title">Plant Care</h1>
+                    <p id="home-subtitle">Take care of your green friends</p>
+                </div>
+                <div className="categories">
+                    <div className="category-card">
+                        <h2>Стайни растения</h2>
+                        <div>
+                            <img src="https://tami.bg/wp-content/uploads/2024/02/vechno-zeleni-staini-cvetq-1024x738.webp" alt="home-plants" />
+                        </div>
+                        <Link to="/plants/category/homeplants" className="btn">Виж всички</Link>
+                    </div>
+
+                    <div className="category-card">
+                        <h2>Градински растения</h2>
+                        <div>
+                            <img src="https://home-design.bg/wp-content/uploads/2022/09/echinacea-1024x681.webp" alt="garden-plants" />
+                        </div>
+                        <Link to="/plants/category/gardenplants" className="btn">Виж всички</Link>
+                    </div>
+
+                    <div className="category-card">
+                        <h2>Дървета и храсти</h2>
+                        <div>
+                            <img src="https://gradinarite.com/wp-content/uploads/2015/01/2-Grapapple.jpg" alt="trees-and-shrubs" />
+                        </div>
+                        <Link to="/plants/category/treesandshrubs" className="btn">Виж всички</Link>
+                    </div>
+                </div>
             </div>
-            <div className="cards-grid" id="home-plants">
-                
-            </div>
-        </div>
+        </section>
+
     )
 }
